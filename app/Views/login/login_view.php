8da95f21bd8c6f177c52889678c3190af383fba5
= <=>
2024-03-16 03:00:07 -0400

    login view

--- app/Views/login/login_view.php
+++ app/Views/login/login_view.php
@@ -15,7 +15,7 @@
 <body class="text-blueGray-700 antialiased">
   <main>
     <section class="relative w-full h-full py-40 min-h-screen">
-      <div class="absolute top-0 w-full h-full bg-blueGray-800 bg-full bg-no-repeat" style="background-image: url(../../assets/img/register_bg_2.png)"></div>
+      <div class="absolute top-0 w-full h-full bg-blueGray-800 bg-full bg-no-repeat" style="background-image: url(<?= base_url() ?>assets/img/register_bg_2.png)"></div>
       <div class="container mx-auto px-4 h-full">
         <div class="flex content-center items-center justify-center h-full">
           <div class="w-full lg:w-4/12 px-4">
